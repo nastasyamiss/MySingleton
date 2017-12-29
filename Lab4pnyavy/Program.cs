c13cc95ca52448cbd1ec31c9cd4d0c6b65538232
Anastasia _14 <ganastasia140@gmail.com>
2017-12-29 20:42:59 +0300

    Добавьте файлы проекта.

---
+++ Lab4pnyavy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4pnyavy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton cfgManager1 = Singleton.getInstance();
+            Singleton cfgManager2 = Singleton.getInstance();
+            if (Object.ReferenceEquals(cfgManager1, cfgManager2))
+                Console.WriteLine("Обе ссылки ссылаются на один объект");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+//+ гарантированный только один объект класса
+//+ глобальный доступ
+//+насследование
+//+простота реализации
+//-он сильно затрудняет юнит-тестировани
+//-Потоконебезопасность потоки могут создавать несколько различных объектов.
---
+++ Lab4pnyavy/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4pnyavy
+{
+    class Singleton   //просто класс который гарантирует что будет создан всего 1 его экземпляр
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        { }
+
+        public static Singleton getInstance()
+        {
+            // для исключения возможности создания двух объектов 
+            // при многопоточном приложении
+            if (instance == null)
+                lock (typeof(Singleton))
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+                }
+            return instance;
+        }  
+    }
+}
